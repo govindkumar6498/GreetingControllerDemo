de791852ffe223f3f7db66c1f88714df79802c44
govind6498 <govindsingh6498@gail.com>
2021-11-30 00:30:15 +0530

    ADDED:To initialize project structure

---
+++ src/test/java/com/govind/greetingcontrollerdemo/GreetingControllerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.govind.greetingcontrollerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingControllerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
